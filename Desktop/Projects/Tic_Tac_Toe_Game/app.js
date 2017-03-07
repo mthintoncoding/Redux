4c5864e953992f5212412fbdcea1b19bb221158a
mthintoncoding <mthinton@gmail.com>
2017-03-06 19:01:46 -0500

    Playable board but can not determine winner

---
+++ Desktop/Projects/Tic_Tac_Toe_Game/app.js
@@ -0,0 +1,39 @@
+var X ='<div class="X">X</div>';
+var O ='<div class="O">O</div>';
+
+player_one = 1;
+
+var scoreX = [];
+
+var scoreY = [];
+
+function squareClick(){
+	$('.square').click(function(){
+		if(player_one == 1){
+		$(this).html(X);
+		scoreX.push(this.id);
+		console.log(scoreX);
+		player_one = 0;
+	}
+	else{
+		$(this).html(O);
+		scoreY.push(this.id);
+		console.log(scoreY)
+		player_one = 1;
+	}
+	});
+	
+}
+squareClick();
+
+
+
+function begins_game(){
+$('.begin_game').click(function(event){
+	event.preventDefault();
+	$('.instructions').toggleClass('hidden');
+});
+
+}
+
+$(function(){;begins_game();});
